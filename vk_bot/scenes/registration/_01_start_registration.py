a076b7d02459f8e74af6d42a0b3b5c839dc22441
DeadSky <deadsky.1136@gmail.com>
2025-10-01 16:08:34 +0300

    debug: trying to remove keyboard after button pressed #2

--- vk_bot/scenes/registration/_01_start_registration.py
+++ vk_bot/scenes/registration/_01_start_registration.py
@@ -9,7 +9,7 @@ from services.api_service import get_text_from_db
 def init(labeler: BotLabeler):
     @callback_handler(labeler, cmd="registration", state=Registration.REGISTRATION_START.value)
     async def start_registration(event: MessageEvent):
-        await event.edit_message(keyboard=None)
+        await event.send_empty_answer(keboard=None)
         user_agreement_message = await get_text_from_db("user_agreement_message")
         agreement_menu = make_user_agreement_menu(
             for_state=Registration.REGISTRATION_AGREEMENT.value
