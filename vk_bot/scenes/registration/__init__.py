5da8d5787e4ad59d0841d6e0ab4eeaeb65529a85
DeadSky <deadsky.1136@gmail.com>
2025-10-01 17:56:23 +0300

    fix: fixed some registration imports

--- vk_bot/scenes/registration/__init__.py
+++ vk_bot/scenes/registration/__init__.py
@@ -4,8 +4,9 @@ from . import (
     _01_start_registration,
     _02_user_agreement,
     _03_is_museum_worker,
-    _05_how_much_years,
-    _04b_occupation_input
+    _04a_museum_name,
+    _04b_occupation_input,
+    _05_how_much_years
 )
 
 registration_labeler = BotLabeler()
@@ -14,7 +15,8 @@ registration_labeler = BotLabeler()
 _01_start_registration.init(registration_labeler)
 _02_user_agreement.init(registration_labeler)
 _03_is_museum_worker.init(registration_labeler)
-_04a_how_much_years.init(registration_labeler)
+_04a_museum_name.init(registration_labeler)
 _04b_occupation_input.init(registration_labeler)
+_05_how_much_years.init(registration_labeler)
 
 __all__ = ["registration_labeler"]
